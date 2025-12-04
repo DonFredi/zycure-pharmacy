7d260787460e78cd256e86c7ea05634bfdc0d835
DonFredi <dondadi.fn@gmail.com>
2025-12-04 12:20:41 +0300

    hero fixes

--- src/app/home/components/section/SpecialOffersSection.tsx
+++ src/app/home/components/section/SpecialOffersSection.tsx
@@ -2,6 +2,7 @@ import ProductCard from "@/app/products/components/card/ProductCard";
 import SectionContainer from "@/components/section/SectionContainer";
 import Title from "@/components/ui/Title";
 import { products } from "@/data/products";
+import { MoveRight } from "lucide-react";
 import Link from "next/link";
 
 const SpecialOffersSection = () => {
@@ -16,9 +17,13 @@ const SpecialOffersSection = () => {
           color="text-foreground"
           className="text-xl font-bold"
         />
-        <Link href="/products"> View More </Link>
+        <Link href="/products" className="text-primary hover:underline flex flex-row gap-1 items-center">
+          {" "}
+          View More Products
+          <MoveRight />
+        </Link>
       </div>
-      <div className="flex flex-wrap gap-2 px-4 py-2">
+      <div className="flex flex-wrap gap-4 px-4 py-2 items-center">
         {products.map((product) => (
           <ProductCard key={product.id} {...product} />
         ))}
--- src/app/products/components/card/ProductCard.tsx
+++ src/app/products/components/card/ProductCard.tsx
@@ -5,14 +5,18 @@ import Image from "next/image";
 const ProductCard = ({ image, title, caption, price }: Product) => {
   return (
     <div className="w-[280px] h-[400px] flex flex-col gap-2 items-center border rounded-sm">
-      <Image src={image} alt={title} width={300} height={160} />
-      <span>{caption}</span>
-      <span>{title}</span>
-      <span>{price}</span>
-      <Button>Add to Cart</Button>
-      <Button variant={"outline"} className="text-primary outline outline-primary">
-        View Product
-      </Button>
+      <div className="border-b-2 flex w-fit">
+        <Image src={image} alt={title} width={300} height={240} />
+      </div>
+      <div className="flex flex-col items-center gap-2 px-4 py-2">
+        <span>{caption}</span>
+        <span className="font-bold">{title}</span>
+        <span className="text-primary font-semibold">{price}</span>
+        <Button>Add to Cart</Button>
+        <Button variant={"outline"} className="text-primary outline outline-primary">
+          View Product
+        </Button>
+      </div>
     </div>
   );
 };
