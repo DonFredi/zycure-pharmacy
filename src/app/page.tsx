72a167bd1cece3cff401760a3e9c64332402de61
DonFredi <dondadi.fn@gmail.com>
2025-12-15 09:01:44 +0300

    homepage fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,7 @@
-import Hero from "./home/components/section/Hero";
-import FeaturesSection from "./home/components/section/FeaturesSection";
-import CTASection from "./home/components/section/CTASection";
-import CategorySection from "./home/components/section/CategorySection";
-import ProductSection from "./home/components/section/ProductSection";
-import TrendingProducts from "./home/components/section/TrendingProducts";
-import SpecialOffersSection from "./home/components/section/SpecialOffersSection";
-import ContactSection from "./contacts/components/section/ContactSection";
-import { useProducts } from "@/hooks/useProducts";
+import HomeClient from "./HomeClient";
 
 const Home = () => {
-  const { products, loading } = useProducts();
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-  return (
-    <div>
-      <Hero />
-      <FeaturesSection />
-      <CTASection />
-      <CategorySection />
-      <ProductSection products={products} />
-      <TrendingProducts />
-      <SpecialOffersSection />
-      <ContactSection />
-    </div>
-  );
+  return <HomeClient />;
 };
 
 export default Home;
