c0f645fe6ceabd13306e191a8c226f557f470f5b
DonFredi <dondadi.fn@gmail.com>
2025-12-15 09:41:03 +0300

    product

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,17 +2,23 @@
 import ProductsSection from "./components/section/ProductsSection";
 import CategoryBar from "./components/section/CategoryBar";
 import SectionContainer from "@/components/section/SectionContainer";
+import { Product } from "@/types/products";
 
-const page = () => {
+async function getProducts(): Promise<Product[]> {
+  const res = await fetch(`${process.env.API_URL}/products`);
+  if (!res.ok) throw new Error("Failed to fetch products");
+  return res.json();
+}
+
+export default async function page() {
+  const products = await getProducts();
   return (
     <SectionContainer>
       <h1>This is the Products page</h1>
       <div className="flex flex-row w-full justify-beteeen gap-8">
         <CategoryBar />
-        <ProductsSection />
+        <ProductsSection products={products} />
       </div>
     </SectionContainer>
   );
-};
-
-export default page;
+}
