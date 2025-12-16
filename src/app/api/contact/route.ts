01f4641da6d7f8c58776fe9cbb878e389d140f36
DonFredi <dondadi.fn@gmail.com>
2025-12-16 15:48:30 +0300

    keys

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,6 +1,7 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY!);
+console.log("RESEND_API_KEY:", process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
   try {
